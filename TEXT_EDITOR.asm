10019f620b80bce6985a4a14e2287c8226e244c7
Víctor Hugo Carreón Pulido <hv_carreon@hotmail.com>
2022-05-29 00:31:16 -0500

    Update TEXT_EDITOR.asm

--- TEXT_EDITOR.asm
+++ TEXT_EDITOR.asm
@@ -7,32 +7,82 @@
     curr_line   db 2                ;rows/lines while editing
     curr_char   db 0                ;columns/chars while editing
     ;for main menu
-    deco1       db '  =================================================$'
-    deco2       db '||            Command Line Text Editor             ||$'
-    deco3       db '||                                                 ||$'
-    deco4       db '||         ESC = Exit || CTRL+S = Save File        ||$'
-    deco5       db '||              ARROW KEYS = Navigate              ||$'
-    deco6       db '  =================================================$'
-    docPrompt   db 'Enter Document Name (.txt): $'
+    
+    Titulo      db ' Editor de Texto $'
+    deco1       db '**************************************************$'
+    deco2       db '*                                                *$'
+
+    CNTRLS1     db 'ESC = Salir || CTRL+S = Guardar$'
+    CNTRLS2     db 'ARROW KEYS = Navigate$'
+    
+    INFO1     db 'Carreon Pulido Victor Hugo - 192310436$'
+    INFO2     db 'Instituto Tecnologico Superior de Lerdo$'
+    
+    OPC1     db '1.-Crear Archivo$'
+    OPC2     db '2.-Abrir Archivo$'
+    OPC3     db '3.-Editar Archivo$'
+    OPC4     db '4.-Eliminar Archivo$'    
+    OPC5     db '5.-Salir$'    
+    
+    opcPrompt   db 'Eliga una opcion:$'
+    docPrompt   db 'Ingrese el nombre del documento (.txt): $'
+    
     docName     dw 50 dup(?),'$'
-    openPrompt  db 'Enter Document Name to Open: $'
+    docName2     dw 50 dup(?),'$'  
+    
+    
+    
+    openPrompt  db 'Ingrese el nombre del documento: $'
+
+    delPrompt  db 'Ingrese el nombre del documento a eliminar: $'
+    MSMDEL  db 'Archivo eliminado exitosamente $'
+    ERRORDEL  db 'El archivo no pudo ser eliminado $'
+    
     HANDLE      dw ? 
     header      db 80 dup('='),'$'    
     color       db 3*15+15
     
           
-.CODE 
+.CODE
 
 ;=========== MACROS ===========
+
+CONFIGURAR MACRO
+    
+      MOV AX,0600H;Limpiar pantalla... AL = 00H limpia todos los renglones
+      MOV BH,5BH  ;color de fondo yletra
+      MOV CX,0000H ;esquina sup.izquierda de la pantalla(0,0)
+      MOV DX,184FH ;esquina inf. derecha de la pantalla(24,79)
+      INT 10H
+      
+      MOV AH,02H 
+      MOV DX,00H ;posiscion del cursor(0,0) despues de limpiar pantalla
+      MOV BH,00H  ;Pagina 0
+      INT 10H
+    
+ENDM
+
+FLUSH MACRO
+        
+    mov ax, 0000h
+    mov bx, 0000h
+    mov cx, 0000h
+    mov DX, 0000h
+    
+ENDM
+ 
 newline macro
+    
     mov dl, 10       ;newline ASCII
     mov ah, 2
     int 21h   
     mov dl, 13       ;linefeed (return) ASCII
     mov ah, 2
     int 21h
+    
 endm
 remove macro
+    
     mov dx, 8        ;backspace to go back one char
     mov ah, 2
     int 21h
@@ -42,13 +92,18 @@ remove macro
     mov dx, 8        ;backspace to go back at removed char position
     mov ah, 2
     int 21h
+    
 endm
+
 goto_pos macro row, col
+    
     mov ah, 02h      ;set text position in middle screen
     mov dh, row
     mov dl, col
     int 10h
+    
 endm
+
 clrScrn macro
     mov ah, 02h    ;set cursor to upper left corner
     mov dh, 0
@@ -65,49 +120,105 @@ debug macro arg
     int 21h
 endm
 
-;=============== PROCEDURES ===============
-start_menu proc
-    ;DISPLAY MAIN MENU
-    goto_pos 5, 12
-    mov dx, offset deco1      ;decoration 1
-    mov ah, 9
-    int 21h
-    goto_pos 6, 12
-    mov dx, offset deco2      ;decoration 2
-    mov ah, 9
-    int 21h
-    goto_pos 7, 12
-    mov dx, offset deco3      ;decoration 3
-    mov ah, 9
-    int 21h
-    goto_pos 8, 12
-    mov dx, offset deco4      ;decoration 4
-    mov ah, 9
-    int 21h
-    goto_pos 9, 12
-    mov dx, offset deco5      ;decoration 5
-    mov ah, 9
-    int 21h
-    goto_pos 10, 12
-    mov dx, offset deco6      ;decoration 6
-    mov ah, 9
-    int 21h
-    goto_pos 13, 12
-    mov dx, offset docPrompt  ;prompt doc name field
-    mov ah, 9
-    int 21h
+
+IMPRIMIR MACRO F,C,texto
     
-    ;INPUT CHARS IN DOC NAME FIELD 
-    mov cx, 0  ;array size counter
+    ;FUNCION PARA IMPRIMIR UN TEXTO, DADAS UNAS COORDENADAS EN PANTALLA
+    ;F es para la posicion en X
+    ;C es para la posicion en Y
+    MOV AH,02H
+    MOV DH,F
+    MOV DL,C
+    INT 10H 
+    MOV dx,offset texto
+    MOV ah,09h
+    INT 21h
+    
+ENDM
+
+Macro MARCO
+     
+     Imprimir 3,12,deco1
+     Imprimir 4,12,deco2
+     Imprimir 5,12,deco2
+     Imprimir 6,12,deco2
+     Imprimir 7,12,deco2
+     Imprimir 8,12,deco2
+     Imprimir 9,12,deco2
+     Imprimir 10,12,deco2
+     
+     Imprimir 11,12,deco2
+     Imprimir 12,12,deco2
+     Imprimir 13,12,deco2
+     Imprimir 14,12,deco2
+     Imprimir 15,12,deco2
+     Imprimir 16,12,deco2
+     
+     Imprimir 17,12,deco1
+                         
+ENDM
+
+;=============== Procedimientos ===============
+start_menu proc
+    
+    
+    CONFIGURAR
+    MARCO
+    FLUSH 
+    
+    Imprimir 3,27,Titulo
+    
+    Imprimir 14,15,CNTRLS1
+    Imprimir 15,15,CNTRLS2
+    Imprimir 5,15,INFO2
+    Imprimir 6,15,INFO1
+    
+    Imprimir 8,15,OPC1
+    Imprimir 9,15,OPC2
+    Imprimir 10,15,OPC3
+    Imprimir 11,15,OPC4
+    Imprimir 12,15,OPC5
+    
+    Imprimir 19,12,opcPrompt
+    
+    FLUSH   
+
+    MOV AH,01H
+    INT 21H
+       
+    CMP AL,31H
+    JE Crear
+    
+    CMP AL,32H
+    JE OPEN
+    
+    CMP AL,34H
+    JE Eliminar
+    
+    CMP AL,35H
+    JE ENDPRG
+    
+    JMP EXIT 
+    
+start_menu endp    
+    
+Crear proc
+    
+    CONFIGURAR
+    Imprimir 1,2,docPrompt
+
+    ;Entrada de caracteres en el nombre del documento
+    mov cx, 0  ;Contador del tamano del arreglo
     mov si, offset docName
+    
     input_char: 
     mov ah, 1
     int 21h
-    cmp al, 13          ;check if return key hit
+    cmp al, 13          ;Verifica la tecla return
     je return
-    cmp al, 8           ;check if backspace key hit
+    cmp al, 8           ;Verifica la tecla backspace
     je remove_char
-    inc cx              ;increment array size by 1
+    inc cx              ;Incrementa el arreglo en 1
     mov [si], al
     inc si
     jmp input_char
@@ -115,11 +226,11 @@ start_menu proc
     remove_char:
     cmp cx, 0
     je setPos_ret
-    dec cx              ;decrement array size by 1
+    dec cx              ;Decrementa el arreglo en 1
     dec si
     mov [si], 00h
     
-    mov dl, 32          ;for removing char
+    mov dl, 32          ;Para eliminar el caracter
     mov ah, 2           ;
     int 21h             ;
     mov dl, 8           ;
@@ -133,15 +244,84 @@ start_menu proc
     
     return:    ;clear the screen and return procedure
     ret 
-start_menu endp
+    
+Crear endp
+
+
+Eliminar proc
+    
+        mov ah,3eh ;Cierra el archivo
+        int 21h 
+    
+        CONFIGURAR
+        Imprimir 1,2,delPrompt
+        
+        ;Entrada de caracteres en el nombre del documento
+        mov cx, 0  ;Contador del tamano del arreglo
+        mov si, offset docName
+        
+        input_char3: 
+        mov ah, 1
+        int 21h
+        cmp al, 13          ;Verifica la tecla return
+        je enter3
+        cmp al, 8           ;Verifica la tecla backspace
+        je remove_char3
+        inc cx              ;Incrementa el arreglo en 1
+        mov [si], al
+        inc si
+        jmp input_char3
+        
+        remove_char3:
+        cmp cx, 0
+        je setPos_ret3
+        dec cx              ;Decrementa el arreglo en 1
+        dec si
+        mov [si], 00h
+        
+        mov dl, 32          ;Para eliminar el caracter
+        mov ah, 2           ;
+        int 21h             ;
+        mov dl, 8           ;
+        mov ah, 2           ;
+        int 21h             ;
+        jmp input_char3
+        
+        setPos_ret3:
+        goto_pos 13, 40
+        jmp input_char3 
+        
+        
+        enter3:    ;clear the screen and return procedure
+            CONFIGURAR
+            
+            mov ah,41h
+            mov dx, offset docName
+            int 21h 
+            jc ERROREL ;Si hubo error
+            Imprimir 1,2,MSMDEL
+            JMP start_menu
+        
+        ERROREL:    
+        
+            Imprimir 1,2,ERRORDEL
+            MOV AH,01H
+            INT 21H       
+            
+            FLUSH
+            JMP start_menu
+    
+Eliminar endp
+
 
 upper_bar proc
+    
     goto_pos 0 0
-    mov dx, offset docName  ;display DOCNAME on upper corner
+    mov dx, offset docName  ;Muestra el nombre del documento en (0,0)
     mov ah, 9
     int 21h
     goto_pos 1 0
-    mov dx, offset header
+    mov dx, offset header   ;Muestra en separador
     mov ah, 9
     int 21h
     
@@ -165,7 +345,7 @@ MAIN PROC
     
     mov si, offset matrix 
     mov di, offset matrix_2
-    MAIN_LOOP:                                   
+    MAIN_LOOP:
     ; Get keystroke
     mov ah, 00h
     int 16h
@@ -202,13 +382,17 @@ MAIN PROC
     jmp MAIN_LOOP
          
     EXIT:
-    mov ah, 4ch
-    int 21h
+        JMP start_menu
+
+    ENDPRG:
+        MOV AH, 4CH
+        INT 21H
+    
         
     SAVE:
-    mov ah, 3Ch             ;creating a file
-    mov cx, 0               ;read-only file
-    mov dx, offset docName  ;giving name which we took from Main Menu Doc Name Input
+    mov ah, 3Ch             ;Crear el archivo
+    mov cx, 0               ;Archivo de solo lectura
+    mov dx, offset docName  ;Darle el nombre que tomamos de Main Menu
     int 21h                 
     mov ah, 3Dh             ;opening file
     mov al, 1               ;for writing mode
@@ -223,31 +407,32 @@ MAIN PROC
     jmp MAIN_LOOP  
     
     OPEN:
-    goto_pos 22 0    ;go to bottom to write open file prompt
-    mov dx, offset openPrompt
-    mov ah, 9
-    int 21h
-    ;INPUT CHARS IN DOC NAME FIELD 
-    mov cx, 0  ;array size counter
+    
+    CONFIGURAR
+    Imprimir 1,2,openPrompt
+    
+    
+    ;Entrada de caracteres para el nombre del documento
+    mov cx, 0  ;Contador del tamano del arreglo
     mov di, offset docName
     input_char2: 
     mov ah, 1
     int 21h
-    cmp al, 13          ;check if return key hit
+    cmp al, 13          ;Verifica la tecla return
     je return2
-    cmp al, 8           ;check if backspace key hit
+    cmp al, 8           ;Verifica la tecla backspace
     je remove_char2
-    inc cx              ;increment array size by 1
+    inc cx              ;Incremento del arreglo en 1
     mov [di], al
     inc di
     jmp input_char2
     remove_char2:
     cmp cx, 0
     je setPos_ret2
-    dec cx              ;decrement array size by 1
+    dec cx              ;Decremento del arreglo en 1
     dec di
     mov [di], 00h
-    mov dl, 32          ;for removing char
+    mov dl, 32          ;Para eliminar el caracter
     mov ah, 2           ;
     int 21h             ;
     mov dl, 8           ;
@@ -257,24 +442,25 @@ MAIN PROC
     setPos_ret2:
     goto_pos 22, 29
     jmp input_char2
-    return2:            ;clear the screen and return procedure
+    return2:            ;Limpia la pantalla y regresa al procedimiento
     clrScrn
     call upper_bar 
     goto_pos 2, 0           ;set cursor position beneath upper bar
-    mov ah, 0x3d             ;to open files
+    mov ah, 0x3d             ;Para abir archivos
     mov al, 00               ;file handler for reading files
     mov dx, offset docName
     int 21h
-    mov HANDLE, ax           ;setting up handler
-    mov ah, 0x3f             ;function for reading files
+    mov HANDLE, ax           ;Estableciento en handler
+    mov ah, 0x3f             ;Funcion para leer archivos
     mov bx, HANDLE
-    mov cx, 1760             ;how many bytes to write
-    mov dx, offset matrix    ;where to save read data
-    int 21h       
-    mov dx, offset matrix    ;print the text on editor canvas
+    mov cx, 1760             ;Cuantos bytes se van a escribir
+    mov dx, offset matrix    ;Donde se guardara/leera la informacion
+    int 21h      
+    mov dx, offset matrix    ;Imprime el texto en pantalla
     mov ah, 9                ;
-    int 21h                  ;
-    jmp MAIN_LOOP            
+    int 21h
+    FLUSH
+    jmp MAIN_LOOP
            
     UP:
     cmp row, 2
